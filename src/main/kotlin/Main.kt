ad6aaed404fc5652e81a55e69d073c13d6bd2fa0
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-04-27 00:27:43 +0200

    finished to "Lists"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,4 +7,18 @@ fun main(args: Array<String>) {
     var greetingWord:String = "Hello"
     var greetingTarget:String = "World"
     printGreeting(getGreatings(greetingWord,greetingTarget));
+
+    val interstingThings = arrayOf("Kotlin","Programming")
+    println(interstingThings.size)
+    println(interstingThings[0])
+
+    for (interestingThing in interstingThings){
+        println(interestingThing)
+    }
+
+    interstingThings.forEach { it:String -> println(it)}
+
+    interstingThings.forEachIndexed { index, s ->  println("$s is at $index")}
+
+
 }
